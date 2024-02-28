298fcf65b9ec6b972620bfe38f2a3ef3256257dc
Zhaqian Rouf Alfauzi <zhaqianroufa@gmail.com>
2024-02-28 16:07:34 +0700

    new

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,6 +4,82 @@
     <link rel="stylesheet" href="{{ asset('css/auth/login.css') }}">
 @endpush
 
+<style>
+    html,
+    body {
+        height: 100%;
+    }
+
+    body {
+        display: flex;
+        align-items: center;
+        padding-top: 40px;
+        padding-bottom: 90px;
+        /* background: linear-gradient(50deg, , #e0cc8d); */
+        background: #760712;
+    }
+
+    .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+    }
+
+    .form-signin {
+        padding: 20px;
+    }
+
+    .form-signin .form-floating:focus-within {
+        z-index: 2;
+    }
+
+    .form-signin input[type="email"] {
+        margin-bottom: -1px;
+    }
+
+    .form-signin input[type="password"] {
+        /* margin-bottom: 10px; */
+    }
+
+    /* Gaya default untuk layar berukuran lebih kecil dari 1200px */
+    .card-login {
+        width: 100%;
+        background: #e0cc8d;
+    }
+
+    /* Menggunakan media query untuk mengatur lebar menjadi 400px ketika layar lebih besar dari 1200px */
+    @media screen and (min-width: 1200px) {
+        .card-login {
+            width: 400px;
+        }
+    }
+
+    .login-container {
+        background-color: white;
+
+        box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.2);
+        border-radius: 7px;
+        animation: slide-in 0.5s ease-out;
+    }
+
+    .field-icon {
+        position: absolute;
+        right: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+        cursor: pointer;
+    }
+
+    .field-icon.eye::before {
+        content: "\f06e";
+    }
+
+    .field-icon.eye-slash::before {
+        content: "\f070";
+    }
+</style>
+
 @section('content')
     <div class="container">
         <div class="card card-login mt-5 me-lg-5 ms-lg-5">
